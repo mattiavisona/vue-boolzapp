2ae19437803fbd02fcdaf0f47111ee59e1bdd35a
mattiavisona <mattiav2706@gmail.com>
2023-03-21 16:51:38 +0100

    update

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 const { createApp } = Vue;
 
 createApp({
-  // una lista di todo
-  // che posso vedere e eliminare quelli che ho già fatto
+  
 
   data() {
     return {
-      // contacts è un array di oggetti
-      // ogni oggetto contiene i dettagli dell'utente con cui chattiamo
-      // contiene anche un array di messaggi
-        // ogni messaggio contiene la data, il testo e lo stato (inviato o ricevuto)
       
         contacts: [
 
@@ -20,17 +15,17 @@ createApp({
                 messages: [
 
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '25/12/2020',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '25/12/2020',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 16:15:22',
+                        date: '25/12/2020',
                         message: 'Tutto fatto!',
                         status: 'received'
                     },
@@ -42,17 +37,17 @@ createApp({
                 visible: true,
                 messages: [
                     {
-                        date: '20/03/2020 16:30:00',
+                        date: '25/12/2020',
                         message: 'Ciao come stai?',
                         status: 'sent'
                     },
                     {
-                        date: '20/03/2020 16:30:55',
+                        date: '25/12/2020',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
-                        date: '20/03/2020 16:35:00',
+                        date: '25/12/2020',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
                     }
@@ -64,17 +59,17 @@ createApp({
                 visible: true,
                 messages: [
                     {
-                        date: '28/03/2020 10:10:40',
+                        date: '25/12/2020',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
-                        date: '28/03/2020 10:20:10',
+                        date: '25/12/2020',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
-                        date: '28/03/2020 16:15:22',
+                        date: '25/12/2020',
                         message: 'Ah scusa!',
                         status: 'received'
                     }
@@ -86,12 +81,12 @@ createApp({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '25/12/2020',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '25/12/2020',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
                     }
@@ -103,12 +98,12 @@ createApp({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '25/12/2020',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '25/12/2020',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
                     }
@@ -120,17 +115,17 @@ createApp({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '25/12/2020',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '25/12/2020',
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020 15:51:00',
+                        date: '25/12/2020',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
                     }
@@ -142,12 +137,12 @@ createApp({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '25/12/2020',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '25/12/2020',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
                     }
@@ -159,31 +154,30 @@ createApp({
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '25/12/2020',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '25/12/2020',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:51:00',
+                        date: '25/12/2020',
                         message: 'OK!!',
                         status: 'received'
                     }
                 ],
             }
         ],
-        // questa proprietà mi dice quale sia la chat da visualizzare a destra, cambia in abse
-        // al contatto cliccato a sinistra
+        
         activeContactIndex: 0,
 
-        // una proprietà che memorizza il testo inserito dall'utente nell'input
+        
         userMessageInput: '',
 
-        // proprietà che memorizza la ricerca dell'utente tra i contatti
+        
         userSearch: '',
     }
   },
@@ -191,8 +185,7 @@ createApp({
   methods: {
     
     changeActiveContactIndex(contactIndex) {
-      // cambiare la proprietà activeContactIndex
-      // in base a quale contatto abbiamo cliccato
+
       
       this.activeContactIndex = contactIndex;
 
@@ -202,53 +195,38 @@ createApp({
     sendMessage() {
         
         
-        // creare un nuovo messaggio
+        
         const newMessage = {
-            date: '10/01/2020 15:51:00',
+            date: '25/12/2020',
             message: this.userMessageInput,
             status: 'sent',
         };
         
-        // aggiungerlo alla lista dei messaggi nella chat corrente
 
         this.contacts[this.activeContactIndex].messages.push(newMessage);
 
-        // cancellare il contenuto del campo di input
         this.userMessageInput = '';
 
-        // mi aspetto una risposta dal contatto
         const contactReply = {
-            date: '10/01/2020 15:51:00',
-            message: 'ok',
+            date: '25/12/2020',
+            message: 'Daje!',
             status: 'received',
         }
 
-        // aggiungo il messaggio alla lista di chat
+        
 
         setTimeout(() => {
             this.contacts[this.activeContactIndex].messages.push(contactReply);
         }, 1000);
     },
 
-    // funzione che filtra i contatti in base alla ricerca
+    
     contactsSearch() {
 
-        // visualizzare o meno i contatti nella lista in base alla ricerca dell'utente
-
-        // ciclo ogni contatto
-        // controllo la stringa digitata dall'utente
-        // se il nome del contatto contiene questa stringa
-        // la sua proprietà "done" viene settata a true
-        // ALTRTIMENTI
-        // la sua proprietà "done" viene settata a false
+        
 
         this.contacts.forEach(contact => {
-            // contact in questo caso è il contatto singolo dall'array contacts
-            // cambia ad ogni iterazione
-
-            // per permettere una ricerca NON case sensitive, faccio il controllo sulle stringhe
-            // convertite in minuscolo
-
+            
             if(contact.name.toLowerCase().includes(this.userSearch.toLowerCase())) {
                 contact.visible = true;
             } else {
